236452fe5ce995b0c61970fc613aeae4f0f5adf0
Jerry Dobner <jdobner@gmail.com>
2021-03-23 18:13:44 -0400

    added yaml stuff

--- yamlread.py
+++ yamlread.py
@@ -1,4 +1,15 @@
 import yaml
+from pprint import pprint as pr
 
-with open("yaml.yaml", 'r') as fp:
-    d = yaml.load()
+
+def read_and_print(file="yamlread2.yaml") -> {}:
+    with open(file, 'r') as fp:
+        d = yaml.load(fp, Loader=yaml.FullLoader)
+        pr(d)
+        print("\n\n\n")
+        return d
+
+def doit():
+    read_and_print("yamlread.yaml")
+    read_and_print('yamlread2.yaml')
+
